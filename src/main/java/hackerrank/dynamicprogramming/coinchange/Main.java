99599bdf9d79d0d962bdb67e6ded67c2d743c3cc
Elvis de Freitas <edigitalb@gmail.com>
2017-11-26 15:58:15 -0200

    adding fibonacci using dp programming

--- src/main/java/hackerrank/dynamicprogramming/coinchange/Main.java
+++ src/main/java/hackerrank/dynamicprogramming/coinchange/Main.java
@@ -1,5 +1,6 @@
 package hackerrank.dynamicprogramming.coinchange;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -13,8 +14,13 @@ public class Main {
 			coinValues[i] = scanner.nextInt();
 		}
 
+		System.out.println(calcChange(coinValues, changeValue, 0, 0));
 
-		System.out.printf("changeValue=%d, coinValuesSize=%d, coinValues=%s%n", changeValue, coinValuesSize, coinValues);
+		System.out.printf("changeValue=%d, coinValuesSize=%d, coinValues=%s%n", changeValue, coinValuesSize, Arrays.toString(coinValues));
 
+	}
+
+	static int calcChange(int[] coins, int changeValue, int actualChange, int i){
+		return -1;
 	}
 }
---
+++ src/main/java/hackerrank/dynamicprogramming/coinchange/fibonacci/Main.java
@@ -0,0 +1,76 @@
+package hackerrank.dynamicprogramming.coinchange.fibonacci;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.concurrent.*;
+
+/**
+ * @see <a href="http://www.geeksforgeeks.org/program-for-nth-fibonacci-number/">the problem</a>
+ */
+public class Main {
+	public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+
+		final int n = 35;
+
+		final long recursiveTime;
+		{
+			final StopWatch stopWatch = new StopWatch();
+			stopWatch.start();
+
+			final long r = fibonacci(n);
+
+			stopWatch.stop();
+			System.out.printf("m=recursive, r=%d, time=%d%n", r, stopWatch.getNanoTime());
+			recursiveTime = stopWatch.getNanoTime();
+		}
+
+		final long dpTime;
+		{
+			final StopWatch stopWatch = new StopWatch();
+			stopWatch.start();
+
+			final BigInteger r = fibonaccid(n);
+
+			stopWatch.stop();
+			System.out.printf("m=dp, r=%d, time=%d%n", r, stopWatch.getNanoTime());
+			dpTime = stopWatch.getNanoTime();
+		}
+
+		System.out.printf("dp is %dx faster%n", recursiveTime / dpTime);
+
+		System.exit(0);
+	}
+
+	/**
+	 * Given a number n, print n-th Fibonacci Number.
+	 *
+	 * Using dynamic program using top down approach
+	 */
+	private static BigInteger fibonaccid(int n) {
+		if(n <= 1){
+			return BigInteger.valueOf(n);
+		}
+		final BigInteger[] fibos = new BigInteger[n + 1];
+		fibos[0] = BigInteger.valueOf(0);
+		fibos[1] = BigInteger.valueOf(1);
+		for (int i = 2; i <= n; i++) {
+			fibos[i] = fibos[i - 2].add(fibos[i - 1]);
+		}
+//		System.out.println(Arrays.toString(fibos));
+		return fibos[n];
+	}
+
+	/**
+	 * Given a number n, print n-th Fibonacci Number.
+	 *
+	 * Using recursion
+	 */
+	private static long fibonacci(int n) {
+		if(n <= 1){
+			return n;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
