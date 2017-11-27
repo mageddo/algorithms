6462f29d9e4fd8c08d35026d3c25d2cde7e69584
Elvis de Freitas <edigitalb@gmail.com>
2017-11-27 01:18:41 -0200

    fixing unit tests

--- src/test/java/hackerrank/dynamicprogramming/coinchange/MainTest.java
+++ src/test/java/hackerrank/dynamicprogramming/coinchange/MainTest.java
@@ -1,46 +1,71 @@
 package hackerrank.dynamicprogramming.coinchange;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+
+import static java.lang.System.setIn;
 import static utils.TestUtils.getResource;
+import static utils.TestUtils.setAndGetOut;
 
 public class MainTest {
 
 	@Test
 	public void sampleInput0() throws Exception {
 
-//		final ByteArrayOutputStream out = TestUtils.setAndGetOut();
-		System.setIn(getResource(MainTest.class, "/sinput0.txt"));
+		// arrange
+		final ByteArrayOutputStream out = setAndGetOut();
+		setIn(getResource(MainTest.class, "/sinput0.txt"));
 
+		// act
 		Main.main(new String[]{});
+
+		// assert
+		Assert.assertEquals("4\n", new String(out.toByteArray()));
 	}
 
 
 	@Test
 	public void sampleInput1() throws Exception {
 
-//		final ByteArrayOutputStream out = TestUtils.setAndGetOut();
-		System.setIn(getResource(MainTest.class, "/sinput1.txt"));
+		// arrange
+		final ByteArrayOutputStream out = setAndGetOut();
+		setIn(getResource(MainTest.class, "/sinput1.txt"));
 
+		// act
 		Main.main(new String[]{});
+
+		// assert
+		Assert.assertEquals("5\n", new String(out.toByteArray()));
 	}
 
 	@Test
 	public void sampleInput2() throws Exception {
 
-//		final ByteArrayOutputStream out = TestUtils.setAndGetOut();
-		System.setIn(getResource(MainTest.class, "/sinput2.txt"));
+		// arrange
+		final ByteArrayOutputStream out = setAndGetOut();
+		setIn(getResource(MainTest.class, "/sinput2.txt"));
 
+		// act
 		Main.main(new String[]{});
+
+		// assert
+		Assert.assertEquals("1\n", new String(out.toByteArray()));
 	}
 
 	@Test
 	public void testCase09() throws Exception {
 
-//		final ByteArrayOutputStream out = TestUtils.setAndGetOut();
-		System.setIn(getResource(MainTest.class, "/input09.txt"));
+		// arrange
+		final ByteArrayOutputStream out = setAndGetOut();
+		setIn(getResource(MainTest.class, "/input09.txt"));
 
+		// act
 		Main.main(new String[]{});
+
+		// assert
+		Assert.assertEquals("15685693751\n", new String(out.toByteArray()));
 	}
 
 }
