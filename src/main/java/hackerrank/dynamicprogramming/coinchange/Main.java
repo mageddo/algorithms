ebceb11bc03c46865b9bf924f483620d476e6549
Elvis de Freitas <edigitalb@gmail.com>
2017-11-27 00:40:27 -0200

    dynamic programming solution

--- src/main/java/hackerrank/dynamicprogramming/coinchange/Main.java
+++ src/main/java/hackerrank/dynamicprogramming/coinchange/Main.java
@@ -14,42 +14,67 @@ public class Main {
 			coins[i] = scanner.nextInt();
 		}
 
-		System.out.println(calcWays(coins, change, coins.length, ""));
+		System.out.println(calcWaysdp(coins, change, coins.length));
 		System.out.printf("change=%d, coinValuesSize=%d, coins=%s%n", change, coinValuesSize, Arrays.toString(coins));
 
 
 
 	}
 
-	static int calcWays(int[] coins, int change, int i, String c) {
+	static int calcWaysdp(int[] coins, int change, int m) {
 
-		final int coin = i > 0 ? coins[i - 1] : -1;
-		System.out.printf("%scoin=%d, change=%d", c, coin, change);
+		final int[][] table = new int[change + 1][m];
+
+		for (int i = 0; i < m; i++) {
+			table[0][i] = 1;
+		}
+
+		for (int i = 1; i <= change; i++) {
+			for (int j = 0; j < m; j++) {
+
+				final int x,y;
+				if(i - coins[j] >= 0){
+					x = table[i - coins[j]][j];
+				}else{
+					x = 0;
+				}
+
+				if(j >= 1) {
+					y = table[i][j - 1];
+				}else{
+					y = 0;
+				}
+
+				table[i][j] = x + y;
+			}
+		}
+		return table[change][m - 1];
+
+	}
+	static int calcWays(int[] coins, int change, int i) {
+
 		if(change == 0){
-			System.out.println(", r=1");
 			return 1;
 		}
 		if(change < 0){
-			System.out.println(", r=0");
 			return 0;
 		}
 		if(i <= 0){
-			System.out.println(", r=0");
 			return 0;
 		}
-		System.out.println();
+		final int coin = coins[i - 1];
 		return
 			/*
 				Faz a chamada recursiva para todas as moedas possiveis (vide i -1)
 				para que a outra chamada recursiva possa calcular o troco
 			 */
-			calcWays(coins, change, i - 1, c)
+			calcWays(coins, change, i - 1)
 			+
 
 			/*
 				Calcula a sobra do troco com a moeda atual e
 				manda para que seja calculado o troco novamente com a mesma moeda
 			 */
-			calcWays(coins, change - coin, i, c + "\t");
+			calcWays(coins, change - coin, i);
 	}
 }
