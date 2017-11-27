231c59ac66e49f29cc1665bff7e8e9ab51eda74c
Elvis de Freitas <edigitalb@gmail.com>
2017-11-27 00:19:41 -0200

    commenting

--- src/main/java/hackerrank/dynamicprogramming/coinchange/Main.java
+++ src/main/java/hackerrank/dynamicprogramming/coinchange/Main.java
@@ -14,24 +14,42 @@ public class Main {
 			coins[i] = scanner.nextInt();
 		}
 
-		System.out.println(calcWays(coins, change, coins.length));
+		System.out.println(calcWays(coins, change, coins.length, ""));
 		System.out.printf("change=%d, coinValuesSize=%d, coins=%s%n", change, coinValuesSize, Arrays.toString(coins));
 
 
 
 	}
 
-	static int calcWays(int[] coins, int change, int i) {
+	static int calcWays(int[] coins, int change, int i, String c) {
 
+		final int coin = i > 0 ? coins[i - 1] : -1;
+		System.out.printf("%scoin=%d, change=%d", c, coin, change);
 		if(change == 0){
+			System.out.println(", r=1");
 			return 1;
 		}
 		if(change < 0){
+			System.out.println(", r=0");
 			return 0;
 		}
 		if(i <= 0){
+			System.out.println(", r=0");
 			return 0;
 		}
-		return calcWays(coins, change, i - 1) + calcWays(coins, change - coins[i - 1], i);
+		System.out.println();
+		return
+			/*
+				Faz a chamada recursiva para todas as moedas possiveis (vide i -1)
+				para que a outra chamada recursiva possa calcular o troco
+			 */
+			calcWays(coins, change, i - 1, c)
+			+
+
+			/*
+				Calcula a sobra do troco com a moeda atual e
+				manda para que seja calculado o troco novamente com a mesma moeda
+			 */
+			calcWays(coins, change - coin, i, c + "\t");
 	}
 }
